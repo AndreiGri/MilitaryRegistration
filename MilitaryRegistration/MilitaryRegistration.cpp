22a47bb4529b26eb90f99f94e2810e2a5ea93fe1
Andrei Gridin <andreigridin918@gmail.com>
2023-09-21 20:23:55 +0500

    Добавьте файлы проекта.

---
+++ MilitaryRegistration/MilitaryRegistration.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки терминала:
+    system("chcp 1251 > nul");
+
+    int height = 0;
+    int weight = 0;
+    int handsNumber = 2;
+    int legsNumber = 2;
+    int flightHours = 0;
+    string eyeColor = "Зелёный";
+
+    cout << endl;
+    cout << " Введите рост и вес призывника: ";
+    cin >> height >> weight;
+    cout << " Введите количество рук и ног призывника: ";
+    cin >> handsNumber >> legsNumber;
+    cout << " Введите количество налётаных часов и цвет глаз презывника: ";
+    cin >> flightHours >> eyeColor;
+
+    if ((height < 145 || height>165) || (weight < 45 || weight>65) || eyeColor == "Зелёный" || flightHours < 100 || legsNumber != handsNumber) {
+        cout << " Призывник негоден к службе в ВВС!" << endl;
+    }
+    else {
+        cout << " Призывник прошёл отбор!" << endl;
+    }
+
+
+    // Задержка окна консоли:
+    system("pause > nul");
+
+    return 0;
+}
